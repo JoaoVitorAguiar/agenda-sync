899ef5115e7683ca8a0edc13277d758dd90e59ee
Vitor Aguiar <joaovitor4guiar@gmail.com>
2025-12-23 21:15:23 -0300

    feat: implement JWT authentication and Google Auth service in Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,5 @@
+using AgendaSync.Routes;
+using AgendaSync.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors();
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<IJwtProvider>(sp =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+
+    return new JwtProvider(
+        secretKey: config["Jwt:SecretKey"]!,
+        issuer: config["Jwt:Issuer"]!,
+        audience: config["Jwt:Audience"]!,
+        expiryInMinutes: int.Parse(config["Jwt:ExpiryInMinutes"]!)
+    );
+});
+
+builder.Services.AddScoped<IAuthService, GoogleAuthService>();
 
 var app = builder.Build();
 
@@ -17,29 +34,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Map routes
+app.MapAuthRoutes();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
